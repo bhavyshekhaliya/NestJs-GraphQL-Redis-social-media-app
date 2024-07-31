f57021b81d09eabda9e0d3904c68d7e9205f74c0
bhavyshekhaliya <bhavyshekhaliya245@gmail.com>
2024-07-31 22:12:59 +0530

    no changes

--- src/common/common.ts
+++ src/common/common.ts
@@ -5,4 +5,4 @@ import * as bcrypt from 'bcrypt';
 export async function hashData(data: string): Promise<string> {
     // 10 is the number of salt rounds. The higher the number, the more secure the hash.
     return bcrypt.hash(data, 10);
-}
+}  
